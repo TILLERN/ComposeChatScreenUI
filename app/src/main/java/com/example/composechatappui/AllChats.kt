c64d3b39ff4a14c7b99c3e9272f17afbfa18ced2
TILLERN <brysonminodi@google.com>
2022-01-26 00:44:11 +0300

    one chat added

--- app/src/main/java/com/example/composechatappui/AllChats.kt
+++ app/src/main/java/com/example/composechatappui/AllChats.kt
@@ -1,13 +1,38 @@
 package com.example.composechatappui
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 
 @Composable
-fun AllChats(messages:List<Message>){
-    LazyColumn(content = ){
-        items(messages){ message ->
-            MessageCard(message)
+fun AllChats(){
+     MessageCard()
+}
+
+@Composable
+fun MessageCard(){
+    Row(modifier = Modifier.padding(8.dp)) {
+        Image(
+            painter = painterResource(id = R.drawable.blueavatar),
+            contentDescription = "my image",
+            modifier = Modifier
+                .size(40.dp)
+                .clip(CircleShape)
+        )
+
+        Spacer(modifier = Modifier.width(8.dp))
+
+        Column(modifier = Modifier.padding(all = 5.dp)) {
+            Text(text = "Chat Name")
+            Spacer(modifier = Modifier.height(8.dp))
+            Text(text = "Hey this is my message to you uuuu!")
         }
     }
 }
---
+++ app/src/main/java/com/example/composechatappui/ChatRepository.kt
@@ -0,0 +1,51 @@
+package com.example.composechatappui
+
+class ChatRepository {
+    val messages = listOf(
+        Message(
+            "Tillern Composer", "Cloud Apex Meeting", "Hello Tillern, this is a reminder that there will be a cloud apex meeting tomorrow 2pm at Ngoingwa. Please purpose to attend."
+        ) ,
+        Message(
+            "Vue Guru", "Cloud Apex Meeting", "Hello Tillern, this is a reminder that there will be a cloud apex meeting tomorrow 2pm at Ngoingwa. Please purpose to attend."
+        ),
+        Message(
+            "Node Enthusiast", "Cloud Apex Meeting", "Hello Tillern, this is a reminder that there will be a cloud apex meeting tomorrow 2pm at Ngoingwa. Please purpose to attend."
+        ),
+        Message(
+            "Kotlin Dev", "Cloud Apex Meeting", "Hello Tillern, this is a reminder that there will be a cloud apex meeting tomorrow 2pm at Ngoingwa. Please purpose to attend."
+        ),
+        Message(
+            "Angular Avenger", "Cloud Apex Meeting", "Hello Tillern, this is a reminder that there will be a cloud apex meeting tomorrow 2pm at Ngoingwa. Please purpose to attend."
+        ),
+        Message(
+            "Java Dev", "Cloud Apex Meeting", "Hello Tillern, this is a reminder that there will be a cloud apex meeting tomorrow 2pm at Ngoingwa. Please purpose to attend."
+        ) ,
+        Message(
+            "Flutter UIs", "Cloud Apex Meeting", "Hello Tillern, this is a reminder that there will be a cloud apex meeting tomorrow 2pm at Ngoingwa. Please purpose to attend."
+        ),
+        Message(
+            "Jane Doe", "Cloud Apex Meeting", "Hello Tillern, this is a reminder that there will be a cloud apex meeting tomorrow 2pm at Ngoingwa. Please purpose to attend."
+        ),
+        Message(
+            "Garry Clay", "Cloud Apex Meeting", "Hello Tillern, this is a reminder that there will be a cloud apex meeting tomorrow 2pm at Ngoingwa. Please purpose to attend."
+        ),
+        Message(
+            "Mark Craigyl", "Cloud Apex Meeting", "Hello Tillern, this is a reminder that there will be a cloud apex meeting tomorrow 2pm at Ngoingwa. Please purpose to attend."
+        ),
+        Message(
+            "Tillern Tee", "Cloud Apex Meeting", "Hello Tillern, this is a reminder that there will be a cloud apex meeting tomorrow 2pm at Ngoingwa. Please purpose to attend."
+        ) ,
+        Message(
+            "John Doe", "Cloud Apex Meeting", "Hello Tillern, this is a reminder that there will be a cloud apex meeting tomorrow 2pm at Ngoingwa. Please purpose to attend."
+        ),
+        Message(
+            "Austin Lameck", "Cloud Apex Meeting", "Hello Tillern, this is a reminder that there will be a cloud apex meeting tomorrow 2pm at Ngoingwa. Please purpose to attend."
+        ),
+        Message(
+            "Bobby Trapper", "Cloud Apex Meeting", "Hello Tillern, this is a reminder that there will be a cloud apex meeting tomorrow 2pm at Ngoingwa. Please purpose to attend."
+        ),
+        Message(
+            "Abraham Shayaa", "Cloud Apex Meeting", "Hello Tillern, this is a reminder that there will be a cloud apex meeting tomorrow 2pm at Ngoingwa. Please purpose to attend."
+        ),
+    )
+}
